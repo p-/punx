79d5cdea7e1ec28a7d931096ab671aaefdaa4fde
GitHub Action <action@github.com>
2022-01-01 20:59:29 +0000

    Add changes for b3d0e880bd3707307d85e0a9c6087412e61c55dc

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("punx  documentation");
+        $("li.nav-item-0 a").text("punx 0.3.0 documentation");
     }
 });
